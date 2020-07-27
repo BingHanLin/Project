4da1278990c7acf7f25a4dc6c2b84b1b6147b777
Bing Han Lin <jaguar3096@gmail.com>
2020-07-27 23:58:08 +0800

    get cites page link

--- Python - Acdemic Citation Network/test.py
+++ Python - Acdemic Citation Network/test.py
@@ -8,14 +8,23 @@ class ScholarQuerier():
 
         DOISplit = DOI.split("/")
 
-        DOIurl = 'https://scholar.google.com.tw/scholar?hl=zh-TW&as_sdt=0%2C5&q={0}%2F{1}&oq='.format(
+        self._DOIurl = 'https://scholar.google.com.tw/scholar?hl=zh-TW&as_sdt=0%2C5&q={0}%2F{1}&oq='.format(
             DOISplit[0], DOISplit[1])
 
-        res = requests.get(DOIurl)
-        soup = BeautifulSoup(res.text, "html.parser")
-        print(res.text)
+        citesPageUrl = self.getCitesPageUrl()
+        print(citesPageUrl)
 
-        # tag = soup.find(name='form', attrs={'id': 'gs_settings_form'})
+    def getCitesPageUrl(self) -> str:
+        res = requests.get(self._DOIurl)
+        soup = BeautifulSoup(res.text, "html.parser")
+        article = soup.find(id='gs_res_ccl_mid').find(
+            "div", class_="gs_r gs_or gs_scl").find(
+            "div", class_="gs_fl")
+
+        links = article.find_all('a')
+
+        return "https://scholar.google.com.tw{0}".format(links[2].get('href'))
+# tag = soup.find(name='form', attrs={'id': 'gs_settings_form'})
 
 
 if __name__ == '__main__':
