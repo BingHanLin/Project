aa9f0454c60da3dd2670e5d4363bc6f5888ae479
Bing Han Lin <binghan.lin@shoncloud.com>
2020-09-23 10:03:56 +0800

    remove parallel stl

--- CPP_Paralleling-Test/src/app/main.cpp
+++ CPP_Paralleling-Test/src/app/main.cpp
@@ -3,15 +3,16 @@
 #include "threadMismatch.hpp"
 #include "workSharing.hpp"
 
-#include "parSTL.hpp"
+// #include "parSTL.hpp"
 
 // https://software.intel.com/en-us/forums/intel-c-compiler/topic/777017
-int main() {
+int main()
+{
   // threadMismatchTest(5, 1'000'000, 10'000);
   // loopSchedules();
   // loopSchedulesVisualize();
   ompSections();
   ompSingle();
   ompMaster();
-  parSTL(5, 5, 100000);
+  // parSTL(5, 5, 100000);
 }
