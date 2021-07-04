3c215b2f2218a1a89077b6a9e495200068da6d03
Binghan Lin <binghan.lin@shoncloud.com>
2021-07-04 10:28:31 +0800

    add compile definitions

--- CPP_VSCode-Test/Make&Use DLL/exampleDLL/example.hpp
+++ CPP_VSCode-Test/Make&Use DLL/exampleDLL/example.hpp
@@ -1,9 +1,13 @@
 #ifndef EXAMPLE_HPP
 #define EXAMPLE_HPP
 
-extern "C"
-{
-    __declspec(dllexport) double addNumbers(const double& a, const double& b);
-}
+#ifdef EXAMPLELIBRARY_EXPORTS
+#define EXAMPLELIBRARY_API __declspec(dllexport)
+#else
+#define EXAMPLELIBRARY_API __declspec(dllimport)
+#endif
+
+extern "C" EXAMPLELIBRARY_API double addNumbers(const double& a,
+                                                const double& b);
 
 #endif  // EXAMPLE_HPP
--- CPP_VSCode-Test/Make&Use DLL/note.md
+++ CPP_VSCode-Test/Make&Use DLL/note.md
@@ -1,1 +1,5 @@
 http://puremonkey2010.blogspot.com/2010/08/c-export-dll-export-dll.html
+
+https://docs.microsoft.com/zh-tw/cpp/build/walkthrough-creating-and-using-a-dynamic-link-library-cpp?view=msvc-160
+
+https://blog.csdn.net/bytxl/article/details/46313581
